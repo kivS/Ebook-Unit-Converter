e01b4dc3aa7b4421a7aef4b621779797aeeb03e5
kivS <v1k@protonmail.com>
2016-07-04 16:08:57 +0100

    restructured code

--- my.js
+++ my.js
@@ -8,13 +8,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	document.getElementById('files').addEventListener('change',startOwl);
 	
   //Definition of program
-  $owl = require('./owlHelpers/owlVars.js');
+  $config = require('./config.js');
+
+  // Load units bot.
+  $units = require('./myModules/units.js');
 
   // Load converter modules
-  var epub = require('./owlHelpers/epub.js');
+  $epub = require('./myModules/epub.js');
 
-  // Load units bot
-  owlUnitsBot = require('./owlHelpers/owlUnitsBot.js');
 
   /**
    * User inserts ebooks and chooses beetween imperic or metric system. Owl picks up his pen and starts cramming. Users gets his freshly converted ebooks 
@@ -28,11 +29,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var isUserUnitSet = null;
 
         while(isUserUnitSet !== true){
-          $owl.config.user_options.unit = prompt("Change ebook units to? [toMetric = 0 | toImperial = 1]");
-          if($owl.config.user_options.unit === '0') $owl.config.user_options.unit = 'toMetric';
-          if($owl.config.user_options.unit === '1') $owl.config.user_options.unit = 'toImperial';
+          $config.user_options.unit = prompt("Change ebook units to? [toMetric = 0 | toImperial = 1]");
+          if($config.user_options.unit === '0') $config.user_options.unit = 'toMetric';
+          if($config.user_options.unit === '1') $config.user_options.unit = 'toImperial';
 
-          isUserUnitSet = ($owl.config.user_options.unit !== null && $owl.config.user_options.unit.match(/(toMetric|toImperial)$/gi) !== null) ? true:false;
+          isUserUnitSet = ($config.user_options.unit !== null && $config.user_options.unit.match(/(toMetric|toImperial)$/gi) !== null) ? true:false;
 
         }
 
@@ -50,18 +51,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
           switch(true){
             case isEpub :
-              epub.start(files[i]);
+              $epub.start(files[i]);
             break;
 
             default:
-              $owl.infos.error.push('['+files[i].name+'] is not supported by owl');
+              $config.infos.error.push('['+files[i].name+'] is not supported by owl');
             
           }
         }
           
 
     }catch(e){
-      $owl.infos.error.push(e.stack);
+      $config.infos.error.push(e.stack);
       console.error(e.stack)
     }
 
